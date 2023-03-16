f983692d54faddd538b71c4f077d8b3c41093c71
Trugamr <trugamr@live.in>
2023-03-16 17:29:06 +0530

    add start and end time to stream

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const PORT = 4000
 const app = express()
 
 // const url = 'https://www.youtube.com/watch?v=LlN8MPS7KQs'
+// const url = 'https://www.youtube.com/watch?v=xR3V5Ow2dTI'
 const url = 'https://www.youtube.com/watch?v=YRLw55eGMn8'
 
 app.get('/', async (req, res) => {
@@ -18,6 +19,7 @@ app.get('/', async (req, res) => {
   const json = JSON.parse(result)
   const validated = await z
     .object({
+      duration: z.number(),
       formats: z.array(
         z.object({
           acodec: z.string(),
@@ -25,6 +27,7 @@ app.get('/', async (req, res) => {
           url: z.string(),
           quality: z.number().optional(),
           filesize: z.number().nullish(),
+          abr: z.number().optional(),
         }),
       ),
     })
@@ -35,6 +38,7 @@ app.get('/', async (req, res) => {
     .find(f => f.acodec === 'opus' && f.audio_ext === 'webm')
   invariant(format, 'suitable audio format not found')
   invariant(format.filesize, 'audio format should have filesize')
+  invariant(format.abr, 'audio format should have bitrate')
 
   // const location = path.resolve('snow.weba')
   // const stats = await fs.promises.stat(location)
@@ -49,18 +53,21 @@ app.get('/', async (req, res) => {
   // stream.pipe(res)
 
   // prettier-ignore
-  const { stdout } = execa('ffmpeg', [
+  const { stdout, stderr } = execa('ffmpeg', [
     '-i',
     format.url,
     // '-re', // Read at native frame rate
     '-vn', // Disable video processing
     '-c:a', 'libopus', // Set audio codec
+    // '-b:a', `${format.abr * 1_000}`,
     '-f', 'webm', // Set output format
+    '-ss', "0", // Start time in seconds
+    '-to', validated.duration.toString(), // End time in seconds
     '-', // Send output to stdout
   ])
   invariant(stdout, 'ffmpeg stdout should be available')
-
-  // TODO: Make stream seekable
+  invariant(stderr, 'ffmpeg stderr should be available')
+  stderr.pipe(process.stderr)
 
   res.writeHead(200, {
     'content-type': 'audio/webm',
